859c2784c80a74a17f8fba56389eedbf1dc0d963
Peter Woods <woodspeter37@gmail.com>
2025-01-31 12:54:22 +0000

    Typed function signature.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,1 +1,37 @@
 import {Friend, Colleague } from './myTypes'
+import { friends, colleagues } from './01-basics';
+
+function older(f: Friend) : string {
+    f.age += 1
+    return `${f.name} is now ${f.age}` 
+}
+
+function allOlder(fs: Friend[]): string[] {
+    return fs.map(f => {
+      f.age += 1;
+      return `${f.name} is now ${f.age}`;
+    });
+  }
+  
+console.log(allOlder(friends))
+console.log(older(friends[0]))
+
+// Find the colleague with the highest extension number.
+function highestExtension(cs: Colleague[]): Colleague {
+    const result = cs.sort(
+      (c1, c2) => c1.contact.extension - c2.contact.extension
+    );
+    return result[cs.length - 1];
+  }
+  console.log(highestExtension(colleagues.current));
+  
+
+  function addColleague(cs: Colleague[], name: string, department: string, email: string): void {
+    const highestExt = highestExtension(cs).contact.extension;
+    cs.push({ name, department, contact: { email, extension: highestExt + 1 } });
+}
+
+
+  addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
+  console.log(colleagues.current.filter((c) => c.name === "Sheild O Connell"));
+   
