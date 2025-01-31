3a796ae64abf424aa40933a4121e66d13ee16a48
Peter Woods <woodspeter37@gmail.com>
2025-01-31 13:14:57 +0000

    Function to update friend's interests.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -60,3 +60,16 @@ function findFriends(friends: Friend[], criterion: (friend: Friend) => boolean) 
 
 console.log(findFriends(friends, (friend) => friend.name.startsWith('Pa')));
 console.log(findFriends(friends, (friend) => friend.age < 35));
+
+
+
+function addInterest(friend: Friend, interest: string): string[] {
+  if (!friend.interests) {
+      friend.interests = [];
+  }
+  friend.interests.push(interest);
+  return friend.interests;
+}
+
+console.log(addInterest(friends[0], 'Politics'));
+
